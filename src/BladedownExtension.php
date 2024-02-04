a9f67fe6c7dc7c11c7e8e59a7cdac1e6df7c5ea0
Caen De Silva <caen@desilva.se>
2024-02-04 12:46:53 +0100

    Remove discovery placeholders

--- src/BladedownExtension.php
+++ src/BladedownExtension.php
@@ -3,9 +3,6 @@
 namespace Hyde\Bladedown;
 
 use Hyde\Foundation\Concerns\HydeExtension;
-use Hyde\Foundation\Kernel\FileCollection;
-use Hyde\Foundation\Kernel\PageCollection;
-use Hyde\Foundation\Kernel\RouteCollection;
 
 class BladedownExtension extends HydeExtension
 {
@@ -15,23 +12,5 @@ class BladedownExtension extends HydeExtension
         return [
             // Hyde\Bladedown\Pages\MyPage::class,
         ];
-    }
-
-    /** {@inheritDoc} */
-    public function discoverFiles(FileCollection $collection): void
-    {
-        // Interact with the file discovery process directly.
-    }
-
-    /** {@inheritDoc} */
-    public function discoverPages(PageCollection $collection): void
-    {
-        // Interact with the page discovery process directly.
-    }
-
-    /** {@inheritDoc} */
-    public function discoverRoutes(RouteCollection $collection): void
-    {
-        // Interact with the route discovery process directly.
     }
 }
