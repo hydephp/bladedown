6fec51cfd9f20787ab2b395f76001acdd14dcfbc
Caen De Silva <caen@desilva.se>
2024-02-04 12:53:16 +0100

    Support custom layout matter
    Uses https://github.com/hydephp/develop/pull/1517

--- src/BladedownPage.php
+++ src/BladedownPage.php
@@ -10,4 +10,10 @@ class BladedownPage extends MarkdownPage
 {
     public static string $template = 'hyde::layouts/page';
     public static string $fileExtension = '.blade.md';
+
+    /** @experimental Implements RFC https://github.com/hydephp/develop/pull/1517 */
+    public function getBladeView(): string
+    {
+        return $this->matter->get('extends') ?? parent::getBladeView();
+    }
 }
