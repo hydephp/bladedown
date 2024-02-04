86a30f84ed7af294c235dd1785cfad843f5e29e0
Caen De Silva <caen@desilva.se>
2024-02-04 12:58:16 +0100

    Override compile method to use custom compiler

--- src/BladedownPage.php
+++ src/BladedownPage.php
@@ -11,6 +11,12 @@ class BladedownPage extends MarkdownPage
     public static string $template = 'hyde::layouts/page';
     public static string $fileExtension = '.blade.md';
 
+    /** @inheritDoc */
+    public function compile(): string
+    {
+        return (new BladedownCompiler($this))->compile();
+    }
+
     /** @experimental Implements RFC https://github.com/hydephp/develop/pull/1517 */
     public function getBladeView(): string
     {
