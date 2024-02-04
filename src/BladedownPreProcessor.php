bd3055563d9757c874e925ba9c330b2b9409594f
Caen De Silva <caen@desilva.se>
2024-02-04 14:01:14 +0100

    Create BladedownPreProcessor.php

---
+++ src/BladedownPreProcessor.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Bladedown;
+
+/**
+ * Handles preprocessing logic for the Bladedown compiler.
+ *
+ * @see \Hyde\Bladedown\BladedownCompiler
+ */
+class BladedownPreProcessor
+{
+    //
+}
