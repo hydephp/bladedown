c1ae32a366d02ea350a8302e0f2d877e4949fb4c
Caen De Silva <caen@desilva.se>
2024-02-05 14:16:52 +0100

    Refactor test and fix string syntax

--- tests/bin/test.php
+++ tests/bin/test.php
@@ -1,7 +1,21 @@
 #!/usr/bin/env php
 <?php
 
-$strings = [
+function testFileContainsExpectedStrings(string $file, array $strings): void
+{
+    $file = file_get_contents($file);
+
+    foreach ($strings as $string) {
+        if (str_contains($file, $string)) {
+            echo "✔ '$string' found in index.html\n";
+        } else {
+            echo "❌ '$string' not found in index.html\n";
+            exit(1);
+        }
+    }
+}
+
+testFileContainsExpectedStrings(('hyde/_site/index.html'), [
     'Bladedown Test Page',
     'Hello, World!',
     'Example component was called with no message',
@@ -10,16 +24,5 @@ $strings = [
     '<strong>Custom title slot</strong>',
     '<div class="custom">Custom body slot with title</div>',
     '<blockquote class="my-0" style="border-color: rebeccapurple">',
-    'Component with custom <abbr title="HyperText Markup Language">HTML</abbr> slot content"',
-];
-
-$file = file_get_contents('hyde/_site/index.html');
-
-foreach ($strings as $string) {
-    if (str_contains($file, $string)) {
-        echo "✔ '$string' found in index.html\n";
-    } else {
-        echo "❌ '$string' not found in index.html\n";
-        exit(1);
-    }
-}
+    'Component with custom <abbr title="HyperText Markup Language">HTML</abbr> slot content',
+]);
