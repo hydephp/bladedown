3a242ab75e2a47d79dee361b4392dda490b86c58
Caen De Silva <caen@desilva.se>
2024-02-05 12:52:35 +0100

    Create view.php

---
+++ tests/runner/config/view.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'paths' => [
+        resource_path('views'),
+        base_path('_pages'),
+        base_path('../bladedown/tests/runner/resources/views')
+    ],
+
+    'compiled' => env(
+        'VIEW_COMPILED_PATH',
+        realpath(storage_path('framework/views'))
+    ),
+];
